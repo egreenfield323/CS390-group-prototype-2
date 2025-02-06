6048c8ce377716318c093d9f5be261495698a3f6
Spencer Smith <spencersmith24@outlook.com>
2025-02-06 13:47:35 -0500

    Create fishObject.gd

---
+++ Fishes/fishObject.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+var fish_dict = {
+	"fish_name": "",
+	"species": "",
+	"worth": 0,
+	"rarity": 0,
+	"spawn_chance": 0.0,
+	"distance": 0.0,
+	"weight": 0.0,
+	"difficulty": 0,
+	"caught": false,
+}
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
