d1cbaa7e44ff0ce27bd2aea3a9ea1e27ffb9de6b
Aidan Bensch <57157106+GamemakerPixel@users.noreply.github.com>
2025-02-06 17:12:15 -0500

    Include bite testing in the demo scene

--- scenes/test/input_demo.gd
+++ scenes/test/input_demo.gd
@@ -23,3 +23,27 @@ func _on_cast_button_button_up() -> void:
 
 func _on_request_motion_pressed() -> void:
 	WebInput.request_access()
+
+
+func _on_start_bite_pressed() -> void:
+	WebInput.hook_set.connect(_on_hook_set)
+	WebInput.start_bite()
+	$BiteAnimation.play("biting")
+
+
+func _on_stop_bite_pressed() -> void:
+	WebInput.stop_bite()
+	WebInput.hook_set.disconnect(_on_hook_set)
+	$BiteAnimation.play("no_bite")
+
+
+func _on_hook_set() -> void:
+	WebInput.hook_set.disconnect(_on_hook_set)
+	$BiteAnimation.animation_finished.connect(_on_animation_finished)
+	$BiteAnimation.play("hook_set")
+
+
+func _on_animation_finished(anim_name: String) -> void:
+	if anim_name == "hook_set":
+		$BiteAnimation.play("no_bite")
+		$BiteAnimation.animation_finished.disconnect(_on_animation_finished)
