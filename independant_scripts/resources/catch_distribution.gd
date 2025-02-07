bb6f1174775832da8902584ee8cc8de5fe60ec60
Aidan Bensch <57157106+GamemakerPixel@users.noreply.github.com>
2025-02-07 14:17:02 -0500

    Start CatchDistribution resource (WIP)

---
+++ independant_scripts/resources/catch_distribution.gd
@@ -0,0 +1,14 @@
+extends Resource
+class_name CatchDistribution
+
+class GaussianDistribution:
+	var mean: float
+	var standard_deviation: float
+	var area: float
+	
+	func _init(
+		new_mean: float = 0.0,
+		new_standard_deviation: float = 1.0,
+		new_area: float = 1.0
+	):
+		pass
