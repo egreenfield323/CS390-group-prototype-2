c3d42996fdca3c206f92abc05ef805f560666ec8
Spencer Smith <spencersmith24@outlook.com>
2025-02-12 23:43:39 -0500

    make it so the fish shadow moves more fishlike

--- Fishes/fish_shadow.gd
+++ Fishes/fish_shadow.gd
@@ -2,36 +2,44 @@ extends CharacterBody2D
 
 @onready var nav := $NavigationAgent2D
 
-@export var HOOK : Node
+@export var HOOK: Node
 @export var SPEED := 200
 @export var ACCEL := 5
+@export var DECEL := 5
 
 var target_position
+var moving := false
 
-func _ready() -> void:
-	pass
-
-func _physics_process(delta: float) -> void:
-	
-	if HOOK != null:
+func _physics_process(delta: float) -> void: 
+	if HOOK and HOOK != null:
 		target_position = HOOK.global_position
 	else:
-		target_position = self.global_position
-	
-	if global_position == target_position:
-		stop_moving()
-	
+		target_position = global_position
+		
+	if global_position == target_position: stop_moving()
+
 	nav.set_target_position(target_position)
 	var direction = (nav.get_next_path_position() - global_position).normalized()
 	
-	velocity = velocity.lerp(direction * SPEED, ACCEL * delta)
+	if moving:
+		velocity = velocity.lerp(direction * SPEED, ACCEL * delta)
+	else:
+		velocity = velocity.lerp(Vector2.ZERO, DECEL * delta)
+	
 	move_and_slide()
 
-func stop_moving():
-	SPEED = 0
-	ACCEL = 0
+func stop_moving() -> void: moving = false
+func start_moving() -> void: moving = true
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	#TODO: Start fish catching
-	target_position = Vector2(0,0)
+	stop_moving()
+	target_position = Vector2.ZERO
 	body.queue_free()
+
+func _on_move_timer_timeout() -> void:
+	start_moving()
+	$StopTimer.start(randf_range(0.2, 0.5))
+
+func _on_stop_timer_timeout() -> void:
+	stop_moving()
+	$MoveTimer.start(randf_range(0.5, 2.0))
